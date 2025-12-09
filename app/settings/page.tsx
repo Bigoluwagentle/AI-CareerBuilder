fba92dd2888aed9bb271839be1eb2d5c099a9ad9
bigoluwagentle <olayiwolaolabode06@gmail.com>
2025-12-09 04:03:14 -0800

    Make some changes

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -7,6 +7,7 @@ export default function Settings(){
                 <Navbar/>
                 <section className="w-[80%] min-h-screen">
                     <article className="px-10">
+                        <h1 className="m-20 font-bold">Settings</h1>
                         <h1 className="m-20 font-bold">Coming Soon....</h1>
                     </article>
                     
